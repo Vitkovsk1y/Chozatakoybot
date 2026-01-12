cf04a19b93ae0b89d4beab172c6d301fee51dd89
nowhynot <kirk3828@gmail.com>
2026-01-12 14:40:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,24 +9,12 @@ with open('token.txt', 'r') as f:
 
 r = sr.Recognizer()
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Отправьте голосовое сообщение для распознавания.')
 
-
 async def audio_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message.voice:
-        file_info = update.message.voice
-        file_id = file_info.file_id
-        new_file = await context.bot.get_file(file_id)
-
         await update.message.reply_text("Распознаю...")
-
-        with sr.AudioFile(await new_file.download_as_bytearray()) as source:
-            audio = r.record(source)
-            text = r.recognize_google(audio, language="ru-RU")
-            await update.message.reply_text(text)
-
 
 def main() -> None:
     application = ApplicationBuilder().token(BOT_TOKEN).build()
@@ -42,4 +30,5 @@ if __name__ == '__main__':
 if __name__ == '__main__':
 
     main()
+
 
