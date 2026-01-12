e7bed4e6ceafe4f22515cb4659db0cf32fe4ce44
nowhynot <kirk3828@gmail.com>
2026-01-12 14:21:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import speech_recognition as sr
 
-BOT_TOKEN = '8384769559:AAGUo36O_nH5UsNQ25qHdWL7vXt-9S5cg1o'
+# Загружаем токен из файла
+with open('token.txt', 'r') as f:
+    BOT_TOKEN = f.read().strip()
+
 r = sr.Recognizer()
 
 
@@ -16,7 +19,9 @@ async def audio_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         file_info = update.message.voice
         file_id = file_info.file_id
         new_file = await context.bot.get_file(file_id)
+
         await update.message.reply_text("Распознаю...")
+
         with sr.AudioFile(await new_file.download_as_bytearray()) as source:
             audio = r.record(source)
             text = r.recognize_google(audio, language="ru-RU")
@@ -27,14 +32,14 @@ def main() -> None:
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.VOICE, audio_handler))
+
     print("Бот запущен...")
     application.run_polling()
 
 
 if __name__ == '__main__':
     main()
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 if __name__ == '__main__':
 
     main()
+
